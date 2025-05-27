fe8b7085988860b33f881e7bd917dc5633ab8710
khoirunnisaxc <raa.khoirunnisa@gmail.com>
2025-05-27 09:23:56 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,4 @@ st.title("🎈 Selamat Datang di Web Zahra")
 st.write(
     "ngodingseru bersama Zahra Khoerunnisa"
 )
-st.image("3346c9cd51aae4e6a5c3eb90bd06b60e.jpg", width=200)
+st.image("IMG-20250510-WA0044.jpg", width=200)
